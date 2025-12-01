5acf07a5aa4274da2e53967b074bab9ad1e83186
Rafet <rafetr@risk.az>
2025-12-01 21:43:48 +0400

    bms first version

---
+++ BookManagmentSystem/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿namespace BookManagmentSystem.Models;
+
+internal class Author
+{
+    private static int _autoIncrementId = 1;
+
+    public Author(string name)
+    {
+        Name = name;
+        Id = _autoIncrementId++;
+    }
+
+    public int Id { get; }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Name}";
+    }
+}
---
+++ BookManagmentSystem/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace BookManagmentSystem.Models;
+
+internal class OrderItem
+{
+    public OrderItem(Book book, int count)
+    {
+        Book = book;
+        Count = count;
+    }
+
+    public Book Book { get; set; }
+    public int Count {  get; set; }
+
+    public override string ToString()
+    {
+        return $"{Book.Name} - {Count} -({Book.Price}x{Count})={Book.Price * Count}";
+    }
+}
